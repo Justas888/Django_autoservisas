ac8dd0303db77030bb060553ab725142d480a456
Justas888 <justenas123@gmail.com>
2025-02-04 09:13:14 +0200

    Commit django 3 uzduotys

--- autoservisas/admin.py
+++ autoservisas/admin.py
@@ -4,10 +4,28 @@ from django.contrib import admin
 
 from .models import Klientas, Marke, Modelis, Automobilis, Paslauga, Uzsakymas, UzsakymoPrekes
 
+class UzsakymoPrekesInline(admin.TabularInline):
+    model = UzsakymoPrekes
+    extra = 2
+
+class UzsakymasAdmin(admin.ModelAdmin):
+    list_display = ('id', 'automobilis', 'sukurimo_data', 'statusas')
+    inlines = [UzsakymoPrekesInline]
+    list_filter = ('statusas',)
+    search_fields = ('automobilis__valst_nr',)
+
+class AutomobilisAdmin(admin.ModelAdmin):
+    list_display = ('klientas', 'modelis', 'valst_nr')
+    list_filter = ('klientas', 'modelis')
+    search_fields = ('valst_nr', 'klientas__vardas')
+
+class PaslaugaAdmin(admin.ModelAdmin):
+    list_display = ('pavadinimas', 'kaina')
+
 admin.site.register(Klientas)
 admin.site.register(Marke)
 admin.site.register(Modelis)
-admin.site.register(Automobilis)
-admin.site.register(Paslauga)
-admin.site.register(Uzsakymas)
+admin.site.register(Automobilis, AutomobilisAdmin)
+admin.site.register(Paslauga, PaslaugaAdmin)
+admin.site.register(Uzsakymas, UzsakymasAdmin)
 admin.site.register(UzsakymoPrekes)
--- autoservisas/models.py
+++ autoservisas/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
 from django.db import models
 
@@ -7,6 +8,10 @@ class Klientas(models.Model):
     vardas = models.CharField('Vardas', max_length=100)
     pavarde = models.CharField('Pavardė', max_length=100)
     email = models.EmailField('Email', max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = 'Klientas'
+        verbose_name_plural = 'Klientai'
 
     def __str__(self):
         return f"{self.vardas} {self.pavarde}"
@@ -21,6 +26,11 @@ class Modelis(models.Model):
     marke = models.ForeignKey(Marke, on_delete=models.CASCADE)
     modelis = models.CharField('Modelis', max_length=100)
 
+    class Meta:
+        verbose_name = 'Modelis'
+        verbose_name_plural = 'Modeliai'
+
+
     def __str__(self):
         return f"{self.marke} {self.modelis}"
 
@@ -31,6 +41,10 @@ class Automobilis(models.Model):
     metai = models.IntegerField('Pagaminimo metai')
     valst_nr = models.CharField('Valstybinis numeris',max_length=20)
 
+    class Meta:
+        verbose_name = 'Automobilis'
+        verbose_name_plural = 'Automobiliai'
+
     def __str__(self):
         return f"{self.valst_nr} {self.modelis} {self.metai}"
 
@@ -39,13 +53,17 @@ class Paslauga(models.Model):
     aprasymas = models.TextField('Paslaugos aprašymas', max_length=1300)
     kaina = models.IntegerField('Paslaugos kaina', default=0)
 
+    class Meta:
+        verbose_name = 'Paslauga'
+        verbose_name_plural = 'Paslaugos'
+
     def __str__(self):
         return f"Paslauga: {self.pavadinimas}, kaina: {self.kaina}"
 
 
 
 class Uzsakymas(models.Model):
-    sukurimo_data = models.DateTimeField('Sukurimo data', null=True, blank=True)
+    sukurimo_data = models.DateTimeField('Sukurimo data', default=datetime.now, null=True, blank=True)
     automobilis = models.ForeignKey(Automobilis, on_delete=models.CASCADE)
     imoketa_suma = models.IntegerField('Įmokėta suma')
 
@@ -67,21 +85,33 @@ class Uzsakymas(models.Model):
                                  blank=True,
                                  help_text='Užsakymo statusas')
 
-    @property
-    def bendra_suma(self):
-        viso = sum([item.paslauga.kaina * item.kiekis for item in self.uzsakymo_prekes.all()])
-        return viso
+    # @property
+    # def bendra_suma(self):
+    #     viso = sum([item.paslauga.kaina * item.kiekis for item in self.uzsakymo_prekes.all()])
+    #     return viso
 
+    class Meta:
+        verbose_name = 'Uzsakymas'
+        verbose_name_plural = 'Uzsakymai'
 
+    def __str__(self):
+        return f"{self.automobilis} {self.sukurimo_data}"
 
 class UzsakymoPrekes(models.Model):
     kiekis = models.IntegerField()
     uzsakymas = models.ForeignKey(Uzsakymas, on_delete=models.CASCADE, related_name="uzsakymo_prekes")
     paslauga = models.ForeignKey(Paslauga, on_delete=models.CASCADE)
 
-    @property
-    def paslaugu_kaina(self):
-        return self.paslauga.kaina * self.kiekis
+    # @property
+    # def paslaugu_kaina(self):
+    #     return self.paslauga.kaina * self.kiekis
+
+    class Meta:
+        verbose_name = 'Uzsakymo_Preke'
+        verbose_name_plural = 'Uzsakymų_Prekes'
+
+    def __str__(self):
+        return f"{self.uzsakymas} {self.paslauga} {self.kiekis}"
 
 
 
