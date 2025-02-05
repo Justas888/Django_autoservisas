a8daa410a3cc7a3f539f680e86a640618c1dc156
Justas888 <justenas123@gmail.com>
2025-02-05 13:06:52 +0200

    Commit django 4,5 uzduotys

--- autoservisas/urls.py
+++ autoservisas/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('automobiliai/', views.automobiliai, name='automobiliai-all'),
+    path('automobiliai/<int:automobilis_id>/',
+         views.automobilis, name='automobilis-info'),
+    path('uzsakymai/',
+         views.UzsakymaiListView.as_view(), name='uzsakymai-list'),
+    path('uzsakymas/<int:pk>/',
+        views.UzsakymasDetailView.as_view(), name='uzsakymas-detail'),
 
 ]
--- autoservisas/views.py
+++ autoservisas/views.py
@@ -1,7 +1,46 @@
-from django.shortcuts import render
+from lib2to3.fixes.fix_input import context
+
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import HttpResponse
+from django.views import generic
+from .models import Uzsakymas, Automobilis, Paslauga
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Sveiki, atvykę į Autoserviso pagrindinį puslapį!")
+    paslaugu_kiekis = Paslauga.objects.count()
+    uzsakymu_kiekis = Uzsakymas.objects.count()
+    atliktu_uzsakymu_kiekis = Uzsakymas.objects.filter(statusas=Uzsakymas.COMPLETED).count()
+    automobiliu_kiekis = Automobilis.objects.count()
+
+    context = {
+        'paslaugu_kiekis': paslaugu_kiekis,
+        'uzsakymu_kiekis': uzsakymu_kiekis,
+        'atliktu_uzsakymu_kiekis': atliktu_uzsakymu_kiekis,
+        'automobiliu_kiekis': automobiliu_kiekis
+    }
+
+    return render(request, 'index.html', context=context)
+
+def automobiliai(request):
+    visi_automobiliai = Automobilis.objects.all()
+    context = {'automobiliai': visi_automobiliai}
+    return render(request, 'automobiliai.html', context=context)
+
+def automobilis(request, automobilis_id):
+    automobilis = get_object_or_404(Automobilis, pk=automobilis_id)
+    context = {'automobilis': automobilis}
+    return render(request, 'automobilis.html', context=context)
+
+class UzsakymaiListView(generic.ListView):
+    model = Uzsakymas
+    context_object_name = 'uzsakymai'
+    template_name = 'uzsakymai.html'
+
+    def get_queryset(self):
+        return Uzsakymas.objects.all()
+
+class UzsakymasDetailView(generic.DetailView):
+    model = Uzsakymas
+    context_object_name = 'uzsakymas'
+    template_name = 'uzsakymas.html'
